de7039b52112ec1f4bd601d4e0b469250bd0f170
User <stmuraro@gmail.com>
2021-05-08 20:48:08 -0300

    add download CV button and Open Graph meta tags

--- _nuxt/static/1620001604/es/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1620001604",layout:"default",error:a,serverRendered:b,routePath:"\u002Fes",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},colorMode:{preference:c,value:c,unknown:b}}}(null,true,"system"));
